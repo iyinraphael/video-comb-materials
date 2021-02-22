0976e9f5f0544c4655af8609f42c0a4c4f3d035c
iyinraphael <iyinraphael@gmail.com>
2021-02-21 19:58:31 -0800

    more filtering operators

--- 11-more-filtering-operators/starter/Starter.playground/Contents.swift
+++ 11-more-filtering-operators/starter/Starter.playground/Contents.swift
@@ -5,7 +5,58 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
+example(of: "first(where:)") {
+    let numbers = (1...9).publisher
+    
+    numbers
+        .print()
+        .first(where: { $0 % 2 == 0})
+        .sink(receiveCompletion: {print("Completed with: \($0)")}, receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
 
+example(of: "last(where:)") {
+    let numbers = (1...9).publisher
+    
+    numbers
+        .last(where: { $0 % 2 == 0})
+        .sink(receiveCompletion: { print("Completed with: \($0)")}, receiveValue: {print($0)})
+        .store(in: &subscriptions)
+}
+
+example(of: "last(where:)") {
+    let numbers = PassthroughSubject<Int, Never>()
+    
+    numbers
+        .last(where: { $0 % 2 == 0})
+        .sink(receiveCompletion: { print("Completed with: \($0)")}, receiveValue: {print($0)})
+        .store(in: &subscriptions)
+    
+    numbers.send(1)
+    numbers.send(2)
+    numbers.send(3)
+    numbers.send(4)
+    numbers.send(5)
+    numbers.send(completion: .finished)
+}
+
+example(of: "prefix") {
+    let numbers = (1...10).publisher
+    
+    numbers
+        .prefix(2)
+        .sink(receiveCompletion: {print("Completed with: \($0)")}, receiveValue: { print($0)})
+        .store(in: &subscriptions)
+}
+
+example(of: "drop") {
+    let numbers = (1...10).publisher
+    
+    numbers
+        .drop(while: {$0 % 5 == 0})
+        .sink(receiveValue: {print($0)})
+        .store(in: &subscriptions)
+}
 
 /// Copyright (c) 2020 Razeware LLC
 ///
