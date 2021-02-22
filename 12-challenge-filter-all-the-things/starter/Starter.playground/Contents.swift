203b67ce06a1d57576927ad3e83ca04167e5b2ef
iyinraphael <iyinraphael@gmail.com>
2021-02-21 20:10:48 -0800

    filter challenge

--- 12-challenge-filter-all-the-things/starter/Starter.playground/Contents.swift
+++ 12-challenge-filter-all-the-things/starter/Starter.playground/Contents.swift
@@ -3,7 +3,16 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "filter challenge") {
+    let numbers = (1...100).publisher
+    
+    numbers
+        .dropFirst(50)
+        .prefix(20)
+        .filter({ $0 % 2 == 0})
+        .sink(receiveValue: { print($0)})
+        .store(in: &subscriptions)
+}
 
 /// Copyright (c) 2020 Razeware LLC
 ///
